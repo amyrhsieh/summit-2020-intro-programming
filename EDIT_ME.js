49658c0a6519d4275d3c2bbbf74dbac55304b805
Amy Hsieh <amyrhsieh@users.noreply.github.com>
2020-02-24 16:39:28 -0500

    Remove secret functions from EDIT_ME

--- EDIT_ME.js
+++ EDIT_ME.js
@@ -29,17 +29,3 @@ printOut(dogAge);
 // setBackground, addPuppy, and makeThePuppyDance
 // See EDIT_ME_sample.js for examples
 // ***************************
-theGrandFinale();
-
-// let name = "Amy";
-let color = "azure";
-setTitle("Amy");
-setBackground(color);
-addPuppy("Frenchie", "Stuff You Should Gnaw", 1);
-addPuppy("Puggy", "The Barkfast Club", 1);
-addPuppy("Puggers", "How Stuff Barks", 2);
-addPuppy("Labby", "Pup and Vanished", 4);
-makeThePuppyDance("Frenchie");
-makeThePuppyDance("Puggy");
-makeThePuppyDance("Puggers");
-makeThePuppyDance("Labby");
